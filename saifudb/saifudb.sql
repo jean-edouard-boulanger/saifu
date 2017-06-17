96517e70a53b874094fdd5e2d2b804f65882298c
Jean BOULANGER <jboulanger2@bloomberg.net>
2017-06-17 15:16:59 +0100

    Add db draft

--- saifudb/saifudb.sql
+++ saifudb/saifudb.sql
@@ -4,24 +4,28 @@ CREATE TABLE saifu_users (
     username VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE saifu_portfolios (
+CREATE TABLE saifu_ports (
     id SERIAL PRIMARY KEY,
     user_id int REFERENCES saifu_users(id),
     name VARCHAR(255) NOT NULL,
     description TEXT NULL
 );
 
-CREATE TABLE saifu_portfolio_positions (
-    portfolio_id int REFERENCES saifu_portfolios(id),
-    ticker CHAR(3) NOT NULL,
+CREATE TABLE saifu_port_positions (
+    portfolio_id int REFERENCES saifu_ports(id),
+    ticker VARCHAR(30) NOT NULL,
     size DOUBLE PRECISION NOT NULL CHECK(size >= 0),
     PRIMARY KEY (portfolio_id, ticker)
 );
 
-INSERT INTO saifu_users (id, email, username) VALUES (1, 'jean.edouard.boulanger@gmail.com', 'jean.boulanger');
-INSERT INTO saifu_portfolios (id, user_id, name, description) VALUES (1, 1, 'default', 'Default portfolio');
-INSERT INTO saifu_portfolio_positions (portfolio_id, ticker, size) VALUES (1, 'XRP', 2500.0);
-INSERT INTO saifu_portfolio_positions (portfolio_id, ticker, size) VALUES (1, 'XMR', 30.0);
-INSERT INTO saifu_portfolio_positions (portfolio_id, ticker, size) VALUES (1, 'XML', 14000.0);
-INSERT INTO saifu_portfolio_positions (portfolio_id, ticker, size) VALUES (1, 'ETH', 25.0);
-INSERT INTO saifu_portfolio_positions (portfolio_id, ticker, size) VALUES (1, 'XBT', 0.71865);
+CREATE TABLE saifu_port_price_histo (
+    portfolio_id int REFERENCES saifu_portfolios(id),
+    price DOUBLE PRECISION NOT NULL CHECK(price >= 0),
+    created_at TIMESTAMP
+);
+
+CREATE TABLE saifu_asset_price_histo (
+    ticker VARCHAR(30) NOT NULL,
+    price DOUBLE PRECISION NOT NULL CHECK(price >= 0),
+    quote_time TIMESTAMP
+);
---
+++ saifudb/test_data.sql
@@ -0,0 +1,7 @@
+INSERT INTO saifu_users (id, email, username) VALUES (1, 'jean.edouard.boulanger@gmail.com', 'jean.boulanger');
+INSERT INTO saifu_portfolios (id, user_id, name, description) VALUES (1, 1, 'default', 'Default portfolio');
+INSERT INTO saifu_portfolio_positions (portfolio_id, ticker, size) VALUES (1, 'XRP', 2500.0);
+INSERT INTO saifu_portfolio_positions (portfolio_id, ticker, size) VALUES (1, 'XMR', 30.0);
+INSERT INTO saifu_portfolio_positions (portfolio_id, ticker, size) VALUES (1, 'XML', 14000.0);
+INSERT INTO saifu_portfolio_positions (portfolio_id, ticker, size) VALUES (1, 'ETH', 25.0);
+INSERT INTO saifu_portfolio_positions (portfolio_id, ticker, size) VALUES (1, 'XBT', 0.71865);
